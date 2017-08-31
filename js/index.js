f50ad17c4633c7513f85eda60d48716a54642400
Narut Promsuparoj <chikado_club2@hotmail.com>
2017-09-01 00:26:44 +0700

    Add Homework

---
+++ js/index.js
@@ -0,0 +1,31 @@
+    var isUpper = false;
+    var isShift = false;
+function key(input){
+    let output = document.getElementById("write").textContent;
+    if (isUpper) {
+        output = output + String(input).toLocaleUpperCase();
+        document.getElementById("write").innerHTML = output;
+    }
+    else if (isShift) {
+        output = output + String(input).toLocaleUpperCase();
+        document.getElementById("write").innerHTML = output;
+        isShift = false;
+    }
+    else {
+        output = output + input;
+        document.getElementById("write").innerHTML = output;
+    }
+}
+function del(){
+    let output = document.getElementById("write").textContent;
+    output = output.substring(0, output.length - 1);
+    document.getElementById("write").innerHTML = output;
+}
+function upperCase(){
+    isUpper = true;
+    return isUpper;
+}
+function shift(){
+    isShift = true;
+    return isShift;
+}
